aa07d47c79b30df62317f113eb9ea31b86649de2
FranKiiTM <holasoyfran2003@gmail.com>
2022-04-29 10:02:48 +0200

    Primera version de la clase Pokemon.

--- src/pokemon/Pokemon.java
+++ src/pokemon/Pokemon.java
@@ -1,5 +1,118 @@
 package pokemon;
 
 public class Pokemon {
-    
+    private String nombre;
+    private String mote;
+    private int vitalidad;
+    private int ataque;
+    private int ataqueEspecial;
+    private int velocidad;
+    private int estamina;
+    private int nivel;
+    private int fertilidad;
+    private EnumEstado estado;
+    private EnumTipo tipo;
+
+    public Pokemon(String nombre, String mote, int vitalidad, int ataque, int ataqueEspecial,
+            int velocidad, int estamina, int nivel, int fertilidad, EnumEstado estado, EnumTipo tipo) {
+        this.nombre = nombre;
+        this.mote = mote;
+        this.vitalidad = vitalidad;
+        this.ataque = ataque;
+        this.ataqueEspecial = ataqueEspecial;
+        this.velocidad = velocidad;
+        this.estamina = estamina;
+        this.nivel = nivel;
+        this.fertilidad = fertilidad;
+        this.estado = estado;
+        this.tipo = tipo;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public int getAtaqueEspecial() {
+        return ataqueEspecial;
+    }
+
+    public EnumEstado getEstado() {
+        return estado;
+    }
+
+    public int getEstamina() {
+        return estamina;
+    }
+
+    public int getFertilidad() {
+        return fertilidad;
+    }
+
+    public String getMote() {
+        return mote;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public EnumTipo getTipo() {
+        return tipo;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public int getVitalidad() {
+        return vitalidad;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public void setAtaqueEspecial(int ataqueEspecial) {
+        this.ataqueEspecial = ataqueEspecial;
+    }
+
+    public void setEstado(EnumEstado estado) {
+        this.estado = estado;
+    }
+
+    public void setEstamina(int estamina) {
+        this.estamina = estamina;
+    }
+
+    public void setFertilidad(int fertilidad) {
+        this.fertilidad = fertilidad;
+    }
+
+    public void setMote(String mote) {
+        this.mote = mote;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTipo(EnumTipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public void setVitalidad(int vitalidad) {
+        this.vitalidad = vitalidad;
+    }
 }
