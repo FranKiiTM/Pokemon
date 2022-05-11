5850c23e0784e5fda58618afe5121acbcf1b27dd
FranKiiTM <holasoyfran2003@gmail.com>
2022-05-11 11:05:43 +0200

    MVC v0.1

---
+++ src/modelo/turno/Turno.java
@@ -0,0 +1,37 @@
+package modelo.turno;
+
+import modelo.entrenador.Entrenador;
+import modelo.pokemon.Pokemon;
+
+public class Turno {
+
+    private int numTurno;
+    private Entrenador turnoEntrenador;
+    private Entrenador turnoRival;
+    private Pokemon turnoPokemon;
+
+    public Turno(int numTurno) {
+        this.numTurno = 1;
+    }
+
+    public int getNumTurno() {
+        return numTurno;
+    }
+
+    public void setNumTurno(int numTurno) {
+        this.numTurno = numTurno;
+    }
+
+    public void accionEntrenador() {
+        System.out.println("Entrenador " + turnoEntrenador.getNombre() + ": " + turnoPokemon.getNombre() + " usa "); // TODO:
+                                                                                                                     // añadir
+                                                                                                                     // movimiento
+    }
+
+    public void accionRival() {
+        System.out.println("Entrenador rival" + turnoRival.getNombre() + ": " + turnoPokemon.getNombre() + " usa "); // TODO:
+                                                                                                                     // añadir
+                                                                                                                     // movimiento
+    }
+
+}
