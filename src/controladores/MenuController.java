5772dddadb7f9403c04894d7e99487aa73087ca1
AlvarooCG <102663895+AlvarooCG@users.noreply.github.com>
2022-05-13 01:06:18 +0200

    Configuracion JavaFX

--- src/controladores/MenuController.java
+++ src/controladores/MenuController.java
@@ -1,5 +1,41 @@
 package controladores;
 
-public class MenuController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class MenuController implements Initializable{
+
+    @FXML
+    private Button btnCapturar;
+
+    @FXML
+    private Button btnCombatir;
+
+    @FXML
+    private AnchorPane fondoMenu;
+
+    @FXML
+    private ImageView imgnGengar;
+
+    @FXML 
+    private void capturar(ActionEvent event){
+        System.out.println("hola");
+    }
+
+    @FXML
+    private void combatir(ActionEvent event){
+        System.out.println("hola");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
     
 }
--- src/modelo/main/Main.java
+++ src/modelo/main/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application{
     @Override
     public void start(Stage primaryStage) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("./vistas/MenuPokemon.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("../vistas/MenuPokemon.fxml"));
             Scene scene = new Scene(root);
 
             primaryStage.setScene(scene);
