7942d401e974747d4216cc0da6a2432b1a495b5b
AlvarooCG <102663895+AlvarooCG@users.noreply.github.com>
2022-05-20 00:42:31 +0200

    Implementacion de algunos Pokemon

--- src/App.java
+++ src/App.java
@@ -5,6 +5,9 @@ import modelo.pokemon.Entrenador;
 import modelo.pokemon.EnumEstado;
 import modelo.pokemon.EnumTipo;
 import modelo.pokemon.Movimiento;
+import modelo.pokemon.MovimientoAtaque;
+import modelo.pokemon.MovimientoEstado;
+import modelo.pokemon.MovimientoMejora;
 import modelo.pokemon.Pokemon;
 import modelo.pokemon.Turno;
 
@@ -21,10 +24,30 @@ public class App {
 
         Entrenador entrenador = new Entrenador("Alvaro");
         Combate combate = new Combate();
-        Pokemon poke1 = new Pokemon("Charmander", "Jose Juan", 39, 52, 60, 43, 50, 65, 100, 1, listaMovimientos, 10, EnumEstado.SIN_ESTADO, EnumTipo.FUEGO);
-        Movimiento m1 = new Movimiento() {
-          
-        };
+
+        Pokemon charmander = new Pokemon("Charmander", "Jose Juan", 39, 52, 60, 43, 50, 65, 100, 1, listaMovimientos, 10, EnumEstado.SIN_ESTADO, EnumTipo.FUEGO);
+        MovimientoAtaque m1Charmander = new MovimientoAtaque(EnumTipo.FUEGO, "Ascuas", 5, 40);
+        MovimientoAtaque m2Charmander = new MovimientoAtaque(EnumTipo.FUEGO, "Colmillo Ígneo", 5, 65);
+        MovimientoAtaque m3Charmander = new MovimientoAtaque(EnumTipo.FUEGO, "Lanzallamas", 5, 90);
+        MovimientoAtaque m4Charmander = new MovimientoAtaque(EnumTipo.FUEGO, "Pirotecia", 5, 70);
+
+        Pokemon squirtle = new Pokemon("Squirtle", "Mbappe", 44, 48, 50, 65, 64, 43, 100, 1, listaMovimientos, 10, EnumEstado.SIN_ESTADO, EnumTipo.AGUA);
+        MovimientoAtaque m1Squirtle = new MovimientoAtaque(EnumTipo.AGUA, "Burbuja", 5, 40);
+        MovimientoAtaque m2Squirtle = new MovimientoAtaque(EnumTipo.AGUA, "Pistola agua", 5, 40);
+        MovimientoAtaque m3Squirtle = new MovimientoAtaque(EnumTipo.AGUA, "Acuacola", 5, 90);
+        MovimientoAtaque m4Squirtle = new MovimientoAtaque(EnumTipo.AGUA, "Hidropulso", 5, 60);
+
+        Pokemon phanpy = new Pokemon("Phanpy", "Francis", 90, 60, 40, 60, 40, 40, 100, 1, listaMovimientos, 10, EnumEstado.SIN_ESTADO, EnumTipo.TIERRA);
+        MovimientoAtaque m1Phanpy = new MovimientoAtaque(EnumTipo.AGUA, "Pistola agua", 5, 40);
+        MovimientoAtaque m2Phanpy = new MovimientoAtaque(EnumTipo.TIERRA, "Bofeton lodo", 5, 20);
+        MovimientoAtaque m3Phanpy = new MovimientoAtaque(EnumTipo.TIERRA, "Terremoto", 5, 100);
+        MovimientoAtaque m4Phanpy = new MovimientoAtaque(EnumTipo.TIERRA, "Tierra viva", 5, 90);
+
+        Pokemon caterpie = new Pokemon("Caterpie", "Gusanito", 45, 30, 20, 35, 20, 45, 100, 1, listaMovimientos, 10, EnumEstado.SIN_ESTADO, EnumTipo.BICHO);
+        MovimientoAtaque m1Caterpie = new MovimientoAtaque(EnumTipo.AGUA, "Burbuja", 5, 40);
+        MovimientoMejora m2Caterpie = new MovimientoMejora(EnumTipo.BICHO, "Disparo demora", 5, EnumM)
+        MovimientoAtaque m3Caterpie = new MovimientoAtaque(EnumTipo.BICHO, "Picadura", 5, 90);
+        MovimientoAtaque m4Caterpie = new MovimientoAtaque(EnumTipo.AGUA, "Hidropulso", 5, 60);
 
         int opcion=2;
         do{
--- src/Prueba.java
+++ src/Prueba.java
@@ -1,30 +1,110 @@
+
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class Prueba {
+import com.mysql.cj.jdbc.Blob;
+import com.mysql.cj.xdevapi.Statement;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import modelo.pokemon.Combate;
+import modelo.pokemon.Turno;
+import modelo.pokemon.EnumEstado;
+import modelo.pokemon.Movimiento;
+import modelo.pokemon.Pokemon;
+import modelo.pokemon.EnumTipo;
+
+public class Prueba extends Application {
+
     public static void main(String[] args) {
 
         String url = "jdbc:mysql://localhost:3306/pokemon ";
+
         String login = "root";
+
         String password = "";
 
         try {
+
             Class.forName("com.mysql.cj.jdbc.Driver");
+
             Connection connection = DriverManager.getConnection(url, login, password);
 
             System.out.println("Conexión establecida");
 
+            mostrarPokemon(connection);
+
+            // Pokemon poke = new Pokemon(152, "Chikorita", "Planta", "");
+
+            // insertarPokemon(connection, poke);
+
             try {
+
                 connection.close();
+
                 System.out.println("Conexión cerrada");
+
             } catch (SQLException e) {
+
                 e.printStackTrace();
+
             }
+
         } catch (SQLException sqle) {
+
             sqle.printStackTrace();
+
         } catch (ClassNotFoundException e) {
+
+            e.printStackTrace();
+
+        }
+
+        Combate c = new Combate();
+        Turno t1 = new Turno(1, "Squirte usa pistola agua", "Charmander usa ascuas");
+
+        c.addTurno(t1);
+        c.escribirCombate();
+
+        launch(args);
+
+    }
+
+    public static void mostrarPokemon(Connection connection) throws SQLException {
+        try {
+            String SQL = "SELECT * FROM pokemon";
+            java.sql.Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(SQL);
+
+            while (rs.next()) {
+                System.out.println(rs.getString("Nombre") + ", " + rs.getString("ID_Pokemon"));
+            }
+
+            rs.close();
+            stmt.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("../vistas/menuPokemon/pokemonMenu.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
